d9d8b95b840100d03e0799d4fe55ca1100562125
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2018-09-08 15:02:13 +0900

    styledComponentの導入

--- src/components/SignOut/index.tsx
+++ src/components/SignOut/index.tsx
@@ -1,8 +1,13 @@
 import * as React from "react";
 import { auth } from "../../firebase";
+import styled from "styled-components";
+
+const Button = styled.button`
+  background: red;
+  border-radius: 8px;
+  color: white;
+`;
 
 export const SignOutButton = () => (
-  <button type="button" onClick={auth.signOut}>
-    Sign Out
-  </button>
+  <Button onClick={auth.signOut}>Sign Out</Button>
 );
